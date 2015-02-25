0cdf58b8847ac60b06cac8294444cef43734f721
vpascow <brendongwalter@gmail.com>
2015-02-24 22:56:26 -0500

    removed redundant files

--- lab/dynamicArrays/dynamicArrayComments.c
+++
@@ -1,5 +0,0 @@
-rohail 3:45 PM 
-Brendon: dynamicArray.c
--Nice to see you automatically keep track of the size
--Doubling size of array if you just want to add one element seems like inefficient memory usage (I know the question asks to do this..maybe something to keep in mind for the future?)
--Function level comments should ideally talk about the parameters and the return value 
--- sorting/sort.c
+++
@@ -1,7 +0,0 @@
-#include "sort.h"
-
-void swap(int list[], int i, int j) {
-	int hold = list[i];
-	list[i] = list[j];
-	list[j] = hold;
-}
