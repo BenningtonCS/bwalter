ecd360e4725eea861474f49c365f2f7eacfa2733
vpascow <brendongwalter@gmail.com>
2015-03-03 15:42:43 -0500

    oops, forgot to add comments

--- 2015s-OOP/week2/lab1/problem1/SillyClass.java
+++ 2015s-OOP/week2/lab1/problem1/SillyClass.java
@@ -20,34 +20,71 @@ public class SillyClass {
     public static String magicString = "magic";
     public String otherString;
 
+	/*
+		Parameters: String you want to print
+		Returns: None
+		Print a string to the console
+	*/
     private void printSomething(String thingToPrint) {
         System.out.println(thingToPrint);
     }
 
+	/*
+		Parameters: None
+		Returns: None
+		Print a predetermined secret string
+	*/ 
     private void getSecretString() {
         System.out.println(this.secretString);
     }
 
 
+	/*
+		Silly class constructor
+		Converts the string given to it to uppercase and stores both
+	*/
     public SillyClass(String secret) {
         this.secretString = secret;
         this.otherString = secret.toUpperCase();
     }
 
+	/*
+		Parameters: String you want to print
+		Returns: None
+		Calls printSomething to print your string to the screen
+	*/
     public void callPrintSomething(String something) {
         this.printSomething(something);
     }
 
+	/*
+		Parameters: Number of times you want message to print, message
+		Returns: None
+		Prints your message a number of times
+	*/
     public void callPrintABunchOfTimes(int numTimes, String message) {
         for (int i=0; i<numTimes; i++) {
             this.printSomething(message);
         }
     }
 
+
+	/*
+		Parameters: None
+		Returns: A string
+		Returns a secret string to the user
+	*/
     public String getAString() {
         return this.secretString;
     }
 
+
+	/*
+		Parameters: None
+		Returns: A string
+		Prints out a magic string to the terminal and returns a stored 
+			string
+	*/
     public String printHello() {
         System.out.println(magicString);
         return this.otherString;
