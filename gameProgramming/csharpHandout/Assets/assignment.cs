3149f73276466267d81c35809f57d11150bf18ad
brendon <brendongwalter@gmail.com>
2015-09-10 14:39:21 -0400

    I think I mixed up the goals. goal 4: Change the player's velocity

--- gameProgramming/csharpHandout/Assets/assignment.cs
+++ gameProgramming/csharpHandout/Assets/assignment.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class assignment : MonoBehaviour {
 
+	Rigidbody rbody;
+
 	public string playerName = "Brendon";	// changed to playerName from name to fix
 											// warning CS0108
+
+	public float x = 0;
+	public float z = 0;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Hello, " + playerName);
+		rbody = GetComponent<Rigidbody> ();
 	}
+
+	void FixedUpdate() {
+		x = Input.GetAxis ("Horizontal");
+		z = Input.GetAxis ("Vertical");
+		rbody.velocity = new Vector3(x, 0, z);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
