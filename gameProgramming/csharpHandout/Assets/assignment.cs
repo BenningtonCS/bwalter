57ee403ea9b3f95a641c20fd9bff515b0e6297fe
brendon <brendongwalter@gmail.com>
2015-09-10 14:17:00 -0400

    goal 2: Expose a string to say 'Hello' to anyone

---
+++ gameProgramming/csharpHandout/Assets/assignment.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class assignment : MonoBehaviour {
+
+	public string playerName = "Brendon";	// changed to playerName from name to fix
+											// warning CS0108
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello, " + playerName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
