fab2c69c0c6998507ffa97c827b079b08fbdab76
brendon <brendongwalter@gmail.com>
2015-09-13 23:15:47 -0400

    goal 3: make collectables. Also the weirdest bug ever..

---
+++ gameProgramming/firstGame/Assets/Scripts/Collectables.cs
@@ -0,0 +1,41 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class Collectables : MonoBehaviour {
+
+	public GameObject prefab;
+	public int numObjects = 10;
+	
+	// size of map in x and z
+	public int gridX = 9;	
+	public int gridZ = 9;
+	
+	/*
+		Parameters: integers for the minimum and maxium range
+		Returns: a random integer
+		Generates a random number between min and max
+	*/
+	private int randNumber(int min, int max) {
+		System.Random random = new System.Random();
+		int num = random.Next(min, max);
+		Debug.Log("Random number is " + num); // the right number of objects don't generate unless this is here???
+		return num;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+		// randomly place collectables on the map
+		for (int i=0;i<numObjects;i++) {
+			Vector3 pos = new Vector3(randNumber(-gridX, gridX), 0, randNumber(-gridZ, gridZ));
+			Instantiate(prefab, pos, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ gameProgramming/firstGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	Rigidbody rbody;
+
+	public int speed = 2;
+	public float x = 0;
+	public float z = 0;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+	
+		x = Input.GetAxis ("Horizontal");
+		z = Input.GetAxis ("Vertical");
+		rbody.velocity = new Vector3(x * speed, 0, z * speed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		coll.gameObject.SetActive (false);
+	}
+}
