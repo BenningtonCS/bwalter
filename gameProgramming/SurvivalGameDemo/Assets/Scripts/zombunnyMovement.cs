f88fa2adc095fa1f736df1a6e9b89434feb28dde
brendon <brendongwalter@gmail.com>
2015-09-28 16:08:11 -0400

    well the zombunny moves

---
+++ gameProgramming/SurvivalGameDemo/Assets/Scripts/zombunnyMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombunnyMovement : MonoBehaviour {
+
+	public float movementSpeed = 1f;
+	public float turnSpeed = 100f;
+	
+	Rigidbody zombunnyRigidbody;
+	Animator zombunnyAnimator;
+
+	// Use this for initialization
+	void Start () {
+		zombunnyRigidbody = GetComponent<Rigidbody>();
+		zombunnyAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = transform.position + transform.forward;
+		movement = movement.normalized *Time.deltaTime * movementSpeed;
+		zombunnyRigidbody.MovePosition(transform.position + movement);
+	}
+}
