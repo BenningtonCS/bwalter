d237b0631dc5bd38dc6f31b334f0fc624deda7b1
brendon walter <brendongwalter@gmail.com>
2015-12-02 21:26:40 -0500

    I made minesweeper!

---
+++ gameProgramming/arcadeGame/Assets/Scripts/Board.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	public GameObject boxPrefab;
+	public int numBombs = 20;
+
+	// declare size of board
+	private static int rows = 10;
+	private static int columns = 10;
+
+	// 2d array of the positions of each box
+	private Vector2[,] boxPositions = new Vector2[rows, columns];
+	// 2d array to hold the box game objects
+	private GameObject[,] boxGO = new GameObject[rows, columns];
+	// 2d array to hold locations of each bomb on the board
+	private int[,] bombs = new int[rows, columns];
+
+
+	// Use this for initialization
+	void Start () {
+
+		// initalize bomb array with a bunch of 0s
+		for (int r=0;r<rows;r++) {
+			for (int c=0;c<columns;c++) {
+				bombs[r,c] = 0;
+			}
+		}
+
+		// randomize position of bombs in the array (1 = bomb)
+		for (int i=0;i<numBombs;i++) {
+			int r = Random.Range(0, rows);
+			int c = Random.Range(0, columns);
+			bombs[r, c] = 1;
+		}
+
+		// initialize the board
+		for (int r=0;r<rows;r++) {
+			for (int c=0;c<columns;c++) {
+
+				// place and instantiate the boxes
+				boxPositions[r,c] = new Vector2((float)(c), (float)(r));
+				boxGO[r,c] = (GameObject)Instantiate(boxPrefab, boxPositions[r,c], Quaternion.identity);
+
+				// initialize the bombs from the bomb array
+				if (bombs[r,c] == 1) 
+					boxGO[r,c].GetComponent<Boxes>().isBomb = true;
+
+				// determine the number of bombs adjacent to each box
+				for (int i=-1;i<=1;i++) {
+					for (int j=-1;j<=1;j++) {
+						// make sure it's accessing a valid index
+						if (r+i < 0 || c+j < 0 || r+i >= rows || c+j >= columns)
+							break;
+						// check each square around self for a bomb
+						if (bombs[r+i, c+j] == 1)
+							// increase count for each bomb found
+							boxGO[r,c].GetComponent<Boxes>().adjacentBombs++;
+					}
+				}
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ gameProgramming/arcadeGame/Assets/Scripts/Boxes.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Boxes : MonoBehaviour {
+
+	public bool isBomb;					// is it a bomb?
+	public bool isFlagged = false;		// has the player set a flag?
+	public bool isCleared = false;		// has the box been cleared
+
+	public int adjacentBombs = 0;
+	public GameObject numBombsPrefab;	// text to display adjacentBombs
+
+	private Collider boxCollider;
+
+	// Use this for initialization
+	void Start () {
+		boxCollider = GetComponent<Collider>();
+	}
+
+	// set a flag
+	void setFlag() {
+		isFlagged = true;
+		GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
+	}
+
+	// remove a flag
+	void removeFlag() {
+		isFlagged = false;
+		GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+	}
+
+	// blow up the box and end the game
+	void blowUp() {
+		GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+	}
+
+	// clear the box from the game
+	void clear() {
+		boxCollider.gameObject.SetActive(false);
+		numBombsPrefab = (GameObject)Instantiate(numBombsPrefab, this.transform.position, Quaternion.identity);
+		numBombsPrefab.GetComponent<TextMesh>().text = adjacentBombs.ToString();
+	}
+
+	// called when the player left clicks on a box to remove it
+	void onLeftClick() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		// check which box was hit
+		if (Physics.Raycast(ray, out hit)) {
+			if (hit.collider == boxCollider) {
+				if (isBomb && !isFlagged) blowUp();		
+				else if (!isBomb && !isFlagged) clear();
+			}
+		}
+	}
+
+	// called when the player right clicks on a box to add or remove a flag
+	void onRightClick() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		// check which box was hit
+		if (Physics.Raycast(ray, out hit)) {
+			if (hit.collider == boxCollider) {
+				if (isFlagged) removeFlag();
+				else if (!isFlagged) setFlag();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) onLeftClick();
+		if (Input.GetMouseButtonDown(1)) onRightClick();
+	}
+}
