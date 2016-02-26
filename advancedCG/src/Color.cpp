14f4ce88324145b00dbd95732e95d4b4966eb2fc
brendon walter <brendongwalter@gmail.com>
2016-02-25 21:14:37 -0500

    Create Color class

---
+++ advancedCG/src/Color.cpp
@@ -0,0 +1,36 @@
+class Color {
+
+  protected:
+    float r, g, b, a; // rgba values are between 0 and 1
+
+  public:
+
+    Color() { r=0; g=0; b=0; a=0; }
+    Color(float red, float green, float blue, float alpha) {
+        r = red; g = green; b = blue; a = alpha;
+    }
+
+    /*
+        Setters
+    */
+
+    void setr(float red)   { r = red; }
+    void setg(float green) { g = green; }
+    void setb(float blue)  { b = blue; }
+    void seta(float alpha) { a = alpha; }
+    void setColor(float red, float green, float blue, float alpha) {
+        r = red; g = green; b = blue; a = alpha;
+    }
+
+     /*
+        Getters
+    */
+
+    float getr() { return r; }
+    float getg() { return g; }
+    float getb() { return b; }
+    float geta() { return a; }
+    void getColor(float *red, float *green, float *blue, float *alpha) {
+        *red = r; *green = g; *blue = b; *alpha = a;
+    }
+};
